479a2047e4b7b35922abf0ea57efeb19114d274c
admin <nagarajansai@gmial.com>
2016-04-11 11:45:39 +0530

    Code has been updated on 11-Apr-2016

---
+++ src/testngprj/DataProviderTest.java
@@ -0,0 +1,22 @@
+package testngprj;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderTest {
+	static String loc = "/Users//admin//workspace//testngpk//testdata//cm_api.xls";
+	
+		
+	@DataProvider(name = "getprogram_dp")
+	public Object[][] createData() throws Exception {
+		Excel_processing ex = new Excel_processing();
+		Object[][] retObjArr = ex.getTableArray(loc, "getprograms", "getprograms_tb");
+		return (retObjArr);
+	}	
+	
+  @Test(dataProvider = "getprogram_dp")
+  public void show(String program_id,String program_name) {
+	  System.out.println(program_id);
+	  System.out.println(program_name);
+  }
+}
